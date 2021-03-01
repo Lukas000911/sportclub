38d7082f5efc3e9b0173fe0006f1a357ecda6dad
lkbuin <lkbuin@gmail.com>
2021-03-01 13:29:55 +0200

    config done

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+//DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'Sport');
+
+
+//App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+//URL Root
+define('URLROOT', 'http://localhost/sport/');
+
+//Site Name
+define('SITENAME', 'SportZal');
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once '../app/bootstrap.php';
+
+
+//INIT CORE LIBRUARY
+$init = new Core;
