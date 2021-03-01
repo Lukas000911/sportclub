aabd29c17c91ce4367cbd9af35d127fbe4308117
lkbuin <lkbuin@gmail.com>
2021-03-01 13:33:48 +0200

    controller and pages up

---
+++ app/libraries/Controller.php
@@ -0,0 +1,31 @@
+<?php
+
+
+/* Base App Controller 
+* Loads models and views
+*/
+class Controller
+{
+    //load model
+    public function model($model)
+    {
+        //require model file
+        require_once '../app/models/' . $model . '.php';
+
+        //instantiate model
+        return new $model();
+    }
+
+
+    //Load view
+    public function view($view, $data = [])
+    {
+        //check for the view file
+        if (file_exists('../app/views/' . $view . '.php')) {
+            require_once '../app/views/' . $view . '.php';
+        } else {
+            //View does not exist
+            die('View does not exist');
+        }
+    }
+}
